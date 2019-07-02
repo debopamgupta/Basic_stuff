987dbb9eea52a726b008f5621c7e7ae0a1c51fa1
debopamgupta <debopamgupta3000@gmail.com>
2019-07-02 17:47:37 +0530

    Program to check Case of character entered

---
+++ CaseCheck.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class  CaseCheck
+{
+  public static void main(String[] args)
+  {
+    char c;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a character");
+    c= sc.nextLine().charAt(0);
+    if(Character.isUpperCase(c))
+     System.out.println("UpperCase");
+    else if(Character.isLowerCase(c))
+      System.out.println("LowerCase");
+    else
+    System.out.println("Wrong input character");
+  }
+
+}
